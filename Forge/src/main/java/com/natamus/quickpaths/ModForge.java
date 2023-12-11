ece5a9d7c984f7e0bf3d331000a6e52725f5843f
Rick South <rick@natamus.com>
2023-12-12 00:27:18 +0100

    Quick Paths source code for 1.19.4.

---
+++ Forge/src/main/java/com/natamus/quickpaths/ModForge.java
@@ -0,0 +1,32 @@
+package com.natamus.quickpaths;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.quickpaths.forge.events.ForgePathEvent;
+import com.natamus.quickpaths.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgePathEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
