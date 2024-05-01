0c15d502e6ffc6ece36836c6ee14742f52e815d5
Rick South <rick@natamus.com>
2024-05-01 16:33:26 +0200

    Quick Paths source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/quickpaths/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.quickpaths.util;
+
+public class Reference {
+	public static final String MOD_ID = "quickpaths";
+	public static final String NAME = "Quick Paths";
+	public static final String VERSION = "3.4";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
