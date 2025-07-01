a214a86cdadf927616701d768ab7cb4fb1e9910f
Rick South <rick@natamus.com>
2025-07-01 10:01:39 +0200

    Quick Paths source code for 1.21.7.

---
+++ NeoForge/src/main/java/com/natamus/quickpaths/ModNeoForge.java
@@ -0,0 +1,35 @@
+package com.natamus.quickpaths;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.quickpaths.neoforge.events.NeoForgePathEvent;
+import com.natamus.quickpaths.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgePathEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
