1657fc6a5c71132370afb0f6a024a925a8452237
Rick South <rick@natamus.com>
2026-04-02 16:00:46 +0200

    Quick Paths source code for 26.1.1.

---
+++ Forge/src/main/java/com/natamus/quickpaths/forge/events/ForgePathEvent.java
@@ -0,0 +1,28 @@
+package com.natamus.quickpaths.forge.events;
+
+import com.natamus.quickpaths.events.PathEvent;
+import net.minecraftforge.event.TickEvent.ServerTickEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgePathEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgePathEvent.class);
+	}
+
+	@SubscribeEvent
+	public static void onServerTick(ServerTickEvent.Post e) {
+		PathEvent.onServerTick(e.server());
+	}
+	
+	@SubscribeEvent
+	public static boolean onRightClickGrass(PlayerInteractEvent.RightClickBlock e) {
+		if (!PathEvent.onRightClickGrass(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec())) {
+			return true;
+		}
+		return false;
+	}
+}
